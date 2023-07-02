6c308740ae7473a593cde7e127cd8e5b6309623d
dong2ast <kdh01132006@naver.com>
2023-07-02 14:05:11 +0900

    merge: S3 커밋과 병합

--- src/main/java/org/sophy/sophy/exception/model/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package org.sophy.sophy.exception.model;
-
-import org.sophy.sophy.exception.ErrorStatus;
-
-public class BadRequestException extends SophyException {
-    public BadRequestException(ErrorStatus errorStatus, String message) {
-        super(errorStatus, message);
-    }
-}
