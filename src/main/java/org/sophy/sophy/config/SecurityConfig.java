0ca215787113902dbe1c8d634758695fc30e6779
dong2ast <kdh01132006@naver.com>
2023-07-03 22:42:54 +0900

    add: CD 위해 SecurityConfig 권한 허용 path 추가

--- src/main/java/org/sophy/sophy/config/SecurityConfig.java
+++ src/main/java/org/sophy/sophy/config/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers("/profile/**").permitAll()
                 .antMatchers("/actuator/**").permitAll()
+                .antMatchers("/health/**").permitAll()
                 .anyRequest().authenticated() //나머지 API는 전부 인증 필요
 
                 //JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
