c35cad7c06ee59bdba8f9489c0fbdb06c0594554
dong2ast <kdh01132006@naver.com>
2023-07-05 22:44:19 +0900

    add: 토큰 발급시 클라에서 토큰 만료시간 설정하게끔 변경 + Exception 추가

--- src/main/java/org/sophy/sophy/controller/dto/request/TokenRequestDto.java
+++ src/main/java/org/sophy/sophy/controller/dto/request/TokenRequestDto.java
@@ -12,4 +12,9 @@ public class TokenRequestDto {
     private String accessToken;
     @NotEmpty(message = "Refresh 토큰을 입력해주세요")
     private String refreshToken;
+
+    @NotEmpty(message = "accessToken 만료시간을 설정해주세요.")
+    private Long accessTokenExpiredTime;
+    @NotEmpty(message = "refreshToken 만료시간을 설정해주세요.")
+    private Long refreshTokenExpiredTime;
 }
