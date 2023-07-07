8c93dbea33b32cc6a4c7e20255f99f955a69e3ca
onpyeong <gsh447114@gmail.com>
2023-07-08 03:06:07 +0900

    add: BookCategory, BooktalkStatus, PreliminaryInfo Enum 추가

---
+++ src/main/java/org/sophy/sophy/domain/BooktalkStatus.java
@@ -0,0 +1,10 @@
+package org.sophy.sophy.domain;
+
+public enum BooktalkStatus {
+    //개설 신청/장소 확정/청중 모집 중/모집 마감/완료
+    APPLYING,
+    PLACE_CONFIRMED,
+    RECRUITING,
+    RECRUITING_CLOSED,
+    COMPLETED
+}
