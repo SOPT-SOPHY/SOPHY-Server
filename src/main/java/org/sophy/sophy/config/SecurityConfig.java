7ac290e3a586e31298b34f0571c373b52bc8e945
dong2ast <kdh01132006@naver.com>
2023-07-10 15:24:30 +0900

    chore: cors 제외 정책 적용

--- src/main/java/org/sophy/sophy/config/SecurityConfig.java
+++ src/main/java/org/sophy/sophy/config/SecurityConfig.java
@@ -39,8 +39,8 @@ public class SecurityConfig {
         //CSRF 설정 Disable
         http.csrf().disable()
 
-                .cors().configurationSource(corsConfigurationSource())
-                .and()
+                .cors(cors -> cors.disable())
+//                .and()
 
                 //exception handling 할 때 우리가 만든 클래스를 추가
                 .exceptionHandling()
