0c7970122394d55c9f3cd7726fcf43ddc241196e
onpyeong <gsh447114@gmail.com>
2023-07-10 16:43:53 +0900

    del: MemberBooktalk 연관 관계 편의 메서드 필요 없는 코드 삭제

--- src/main/java/org/sophy/sophy/domain/MemberBooktalk.java
+++ src/main/java/org/sophy/sophy/domain/MemberBooktalk.java
@@ -27,9 +27,6 @@ public class MemberBooktalk extends AuditingTimeEntity {
 
     // 연관 관계 편의 메서드
     public void setMember(Member member) {
-        if(this.member != null) {
-            this.member.getUserBookTalkList().remove(this); //이거 PATCH될 가능성이 있나?
-        }
         this.member = member;
         if(!member.getUserBookTalkList().contains(this)) {
             member.getUserBookTalkList().add(this);
@@ -37,9 +34,6 @@ public class MemberBooktalk extends AuditingTimeEntity {
     }
 
     public void setBooktalk(Booktalk booktalk) {
-        if (this.booktalk != null) {
-            this.booktalk.getParticipantList().remove(this);
-        }
         this.booktalk = booktalk;
         if (!booktalk.getParticipantList().contains(this)) {
             booktalk.getParticipantList().add(this);
