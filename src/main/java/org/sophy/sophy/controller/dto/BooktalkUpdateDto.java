72c4a167c74b1dcd86bf8f4fe7605d6b10fda33f
onpyeong <gsh447114@gmail.com>
2023-07-11 04:11:12 +0900

    add: BooktalkUpdateDto 추가

---
+++ src/main/java/org/sophy/sophy/controller/dto/BooktalkUpdateDto.java
@@ -0,0 +1,37 @@
+package org.sophy.sophy.controller.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.sophy.sophy.domain.BookCategory;
+import org.sophy.sophy.domain.PreliminaryInfo;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class BooktalkUpdateDto {
+    @NotNull
+    private Long placeId;
+    private String booktalkImageUrl;
+    @NotBlank
+    private String title;
+    @NotNull
+    private BookCategory bookCategory;
+    @NotNull
+    private Long bookId; //TODO 추후 연결
+    @NotNull
+    private LocalDateTime startDate; //TODO 시작 시간은 오늘날짜 이전은 안되도록?
+    @NotNull
+    private LocalDateTime endDate;
+    @NotNull
+    private Integer participant;
+    @NotNull
+    private Integer participationFee;
+    @NotNull
+    private PreliminaryInfo preliminaryInfo;
+    @NotBlank
+    private String description;
+}
