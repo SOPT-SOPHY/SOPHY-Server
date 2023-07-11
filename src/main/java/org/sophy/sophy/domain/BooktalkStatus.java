12cdd17816f671df7ee70a5c7b2be1b78fc99557
dong2ast <kdh01132006@naver.com>
2023-07-11 17:57:15 +0900

    merge: #43 BooktalkStatus 주석 추가

--- src/main/java/org/sophy/sophy/domain/BooktalkStatus.java
+++ src/main/java/org/sophy/sophy/domain/BooktalkStatus.java
@@ -2,11 +2,12 @@ package org.sophy.sophy.domain;
 
 public enum BooktalkStatus {
     //개설 신청/장소 확정/청중 모집 중/모집 마감/완료
-    APPLYING,
-    PLACE_REFUSED,
-    PLACE_CONFIRMED,
-    RECRUITING_EXPECTED,
-    RECRUITING,
-    RECRUITING_CLOSED,
-    COMPLETED
+    //매칭 대기중/매칭 거절됨/모집 중/모집 마감/모집 예정
+    APPLYING, //매칭 대기중
+    PLACE_REFUSED, //매칭 거절됨
+    PLACE_CONFIRMED, //장소 확정
+    RECRUITING_EXPECTED, //모집 예정
+    RECRUITING, //모집 중
+    RECRUITING_CLOSED, //모집 마감
+    COMPLETED //완료된 북토크
 }
