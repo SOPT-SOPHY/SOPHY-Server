9ff02bd0c536d2b563f47cb53f55d604296dd012
Suhyeon <70002218+onpyeong@users.noreply.github.com>
2023-07-11 10:49:37 +0900

    feat: 지역으로 북토크 리스트 조회 (#35)
    * modify: 지역으로 공간 조회 RequestBody CityRequestDto로 통일
    
    * feat: 지역으로 북토크 리스트 조회
    
    * fix: @Transitional 추가
    
    * fix: url과 Valid 설정

--- src/main/java/org/sophy/sophy/controller/BooktalkController.java
+++ src/main/java/org/sophy/sophy/controller/BooktalkController.java
@@ -2,47 +2,6 @@ package org.sophy.sophy.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.sophy.sophy.common.dto.ApiResponseDto;
-import org.sophy.sophy.controller.dto.BooktalkUpdateDto;
-import org.sophy.sophy.controller.dto.request.BooktalkRequestDto;
-import org.sophy.sophy.controller.dto.response.BooktalkCreateResponseDto;
-import org.sophy.sophy.controller.dto.response.BooktalkDeleteResponseDto;
-import org.sophy.sophy.controller.dto.response.BooktalkDetailResponseDto;
-import org.sophy.sophy.exception.SuccessStatus;
-import org.sophy.sophy.service.BooktalkService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/booktalk")
-public class BooktalkController {
-    private final BooktalkService booktalkService;
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ApiResponseDto<BooktalkCreateResponseDto> createBooktalk(@Valid @RequestBody BooktalkRequestDto booktalkRequestDto) {
-        return ApiResponseDto.success(SuccessStatus.CREATE_BOOKTALK_SUCCESS, booktalkService.createBooktalk(booktalkRequestDto));
-    }
-
-    @PatchMapping("/{booktalkId}")
-    @ResponseStatus(HttpStatus.OK)
-    public ApiResponseDto<BooktalkUpdateDto> updateBooktalk(@PathVariable("booktalkId") Long booktalkId, @Valid @RequestBody BooktalkUpdateDto booktalkUpdateDto) {
-        return ApiResponseDto.success(SuccessStatus.PATCH_BOOKTALK_SUCCESS, booktalkService.updateBooktalk(booktalkId, booktalkUpdateDto));
-    }
-
-    @DeleteMapping("/{booktalkId}")
-    @ResponseStatus(HttpStatus.OK)
-    public ApiResponseDto<BooktalkDeleteResponseDto> deleteBooktalk(@PathVariable("booktalkId") Long booktalkId) {
-        return ApiResponseDto.success(SuccessStatus.DELETE_BOOKTALK_SUCCESS, booktalkService.deleteBooktalk(booktalkId));
-    }
-
-    @GetMapping("/{booktalkId}/detail")
-    public ApiResponseDto<BooktalkDetailResponseDto> getBooktalkDetail(@PathVariable("booktalkId") Long booktalkId) {
-        return ApiResponseDto.success(SuccessStatus.GET_BOOKTALK_DETAIL_SUCCESS, booktalkService.getBooktalkDetail(booktalkId));
-    }
-
 import org.sophy.sophy.controller.dto.request.CityRequestDto;
 import org.sophy.sophy.controller.dto.response.BooktalkResponseDto;
 import org.sophy.sophy.exception.SuccessStatus;
--- src/main/java/org/sophy/sophy/infrastructure/BooktalkRepository.java
+++ src/main/java/org/sophy/sophy/infrastructure/BooktalkRepository.java
@@ -1,11 +1,6 @@
 package org.sophy.sophy.infrastructure;
 
 import org.sophy.sophy.domain.Booktalk;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BooktalkRepository extends JpaRepository<Booktalk, Long> {
 import org.sophy.sophy.domain.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
