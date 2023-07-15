5066976c643663bc9939ca103c2321991e59063b
dong2ast <kdh01132006@naver.com>
2023-07-15 16:35:24 +0900

    add: 레포에 Id를 통해 엔티티를 받아오는 default 메서드 추가

--- src/main/java/org/sophy/sophy/infrastructure/BooktalkRepository.java
+++ src/main/java/org/sophy/sophy/infrastructure/BooktalkRepository.java
@@ -3,6 +3,8 @@ package org.sophy.sophy.infrastructure;
 import org.sophy.sophy.domain.Booktalk;
 import org.sophy.sophy.domain.enumerate.BooktalkStatus;
 import org.sophy.sophy.domain.enumerate.City;
+import org.sophy.sophy.exception.ErrorStatus;
+import org.sophy.sophy.exception.model.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -17,4 +19,9 @@ public interface BooktalkRepository extends JpaRepository<Booktalk, Long> {
     Integer countAllByCityAndCreateAtBetween(City city, LocalDateTime before, LocalDateTime now);
 
     List<Booktalk> findAllByBooktalkStatus(BooktalkStatus booktalkStatus);
+
+    default Booktalk getBooktalkById(Long booktalkId) {
+        return this.findById(booktalkId)
+                .orElseThrow(() -> new NotFoundException(ErrorStatus.NOT_FOUND_BOOKTALK_EXCEPTION, ErrorStatus.NOT_FOUND_BOOKTALK_EXCEPTION.getMessage()));
+    }
 }
--- src/main/java/org/sophy/sophy/infrastructure/MemberRepository.java
+++ src/main/java/org/sophy/sophy/infrastructure/MemberRepository.java
@@ -1,6 +1,8 @@
 package org.sophy.sophy.infrastructure;
 
 import org.sophy.sophy.domain.Member;
+import org.sophy.sophy.exception.ErrorStatus;
+import org.sophy.sophy.exception.model.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,4 +14,9 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    default Member getMemberById(Long memberId) {
+        return this.findById(memberId)
+                .orElseThrow(() -> new NotFoundException(ErrorStatus.NOT_FOUND_USER_EXCEPTION, ErrorStatus.NOT_FOUND_USER_EXCEPTION.getMessage()));
+    }
 }
