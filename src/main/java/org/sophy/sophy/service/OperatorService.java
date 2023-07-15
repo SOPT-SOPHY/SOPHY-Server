215089871bdc8f578e35129b9459fa4e644562a0
dong2ast <kdh01132006@naver.com>
2023-07-15 21:13:02 +0900

    feat: 공간 운영자 승인 대기중 북토크 조회 및 북토크 승인 메서드 추가

---
+++ src/main/java/org/sophy/sophy/service/OperatorService.java
@@ -0,0 +1,31 @@
+package org.sophy.sophy.service;
+
+import lombok.RequiredArgsConstructor;
+import org.sophy.sophy.domain.Booktalk;
+import org.sophy.sophy.domain.Member;
+import org.sophy.sophy.domain.ScheduledBooktalkConverter;
+import org.sophy.sophy.domain.enumerate.BooktalkStatus;
+import org.sophy.sophy.infrastructure.BooktalkRepository;
+import org.sophy.sophy.infrastructure.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OperatorService {
+
+    private final MemberRepository memberRepository;
+    private final BooktalkRepository booktalkRepository;
+
+    public List<Booktalk> getWaitingBooktalks(Long memberId) { //승인 대기중 북토크 조회
+        Member member = memberRepository.getMemberById(memberId);
+        return member.getOperatorProperty().getRecruitScheduledBooktalks();
+    }
+
+    public void approveBooktalk(Long booktalkId) { //북토크 승인
+        Booktalk booktalk = booktalkRepository.getBooktalkById(booktalkId);
+        booktalk.setBooktalkStatus(BooktalkStatus.RECRUITING_EXPECTED);
+        ScheduledBooktalkConverter.getScheduledBooktalk().add(booktalk); //대기중 큐에 승인된 북토크 추가 -> 다음날 오전 10시에 해당 큐를 이용해 상태 변경
+    }
+}
