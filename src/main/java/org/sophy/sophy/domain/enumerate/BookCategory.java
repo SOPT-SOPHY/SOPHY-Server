bdc84b50f3d7bad269e3ac5623a176ec19cc0628
dong2ast <kdh01132006@naver.com>
2023-07-15 21:09:49 +0900

    add: enum 타입 데이터 추가

--- src/main/java/org/sophy/sophy/domain/enumerate/BookCategory.java
+++ src/main/java/org/sophy/sophy/domain/enumerate/BookCategory.java
@@ -1,9 +1,11 @@
 package org.sophy.sophy.domain.enumerate;
 
 public enum BookCategory {
-    //인문/문학/예술/과학/육아/일상_취미/어린이/청소년/IT_컴퓨터/자기계발/건강_요리/여행
+    //인문/문학/사회/에세이/예술/과학/육아/일상_취미/어린이/청소년/IT_컴퓨터/자기계발/건강_요리/여행
     HUMANITIES,
     LITERATURE,
+    SOCIETY,
+    ESSAY,
     ART,
     SCIENCE,
     PARENTING,
