30547cd1e28694196c2275133894215e7248fffb
dong2ast <kdh01132006@naver.com>
2023-07-25 09:51:33 +0900

    add: enum 권한들 추가

--- src/main/java/org/sophy/sophy/domain/enumerate/Authority.java
+++ src/main/java/org/sophy/sophy/domain/enumerate/Authority.java
@@ -1,5 +1,16 @@
 package org.sophy.sophy.domain.enumerate;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum Authority {
-    ROLE_USER, ROLE_ADMIN
+    ADMIN("ROLE_MEMBER,ROLE_AUTHOR,ROLE_OPERATOR"),
+    MEMBER("ROLE_MEMBER"),
+    AUTHOR("ROLE_MEMBER,ROLE_AUTHOR"),
+    OPERATOR("ROLE_MEMBER,ROLE_OPERATOR"),
+    ALL("ROLE_MEMBER,ROLE_AUTHOR,ROLE_OPERATOR");
+
+    private String value;
 }
