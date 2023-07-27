5456ad42cb711be286ddaf915d45527fdaea8b00
dong2ast <kdh01132006@naver.com>
2023-07-27 17:49:07 +0900

    feat: 소피스토리 API swagger 추가

--- src/main/java/org/sophy/sophy/controller/SophyStoryController.java
+++ src/main/java/org/sophy/sophy/controller/SophyStoryController.java
@@ -1,5 +1,6 @@
 package org.sophy.sophy.controller;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.sophy.sophy.common.dto.ApiResponseDto;
@@ -21,12 +22,12 @@ public class SophyStoryController {
     private final SophyStoryService sophyStoryService;
 
     @GetMapping //소피스토리 연, 월로 조회
-    public ApiResponseDto<List<SophyStoryDto>> geyMySophyStory(@AuthenticationPrincipal User user, @RequestBody SophyStoryRequestDto sophyStoryRequestDto) {
+    public ApiResponseDto<List<SophyStoryDto>> geyMySophyStory(@Parameter(hidden = true) @AuthenticationPrincipal User user, @Parameter @RequestBody SophyStoryRequestDto sophyStoryRequestDto) {
         return ApiResponseDto.success(SuccessStatus.GET_SOPHY_STORY_SUCCESS, sophyStoryService.getMySophyStory(user.getUsername(), sophyStoryRequestDto));
     }
 
     @GetMapping("/all") //소피스토리 모두 조회
-    public ApiResponseDto<List<SophyStoryDto>> geyMySophyStory(@AuthenticationPrincipal User user) {
+    public ApiResponseDto<List<SophyStoryDto>> geyMySophyStory(@Parameter(hidden = true) @AuthenticationPrincipal User user) {
         return ApiResponseDto.success(SuccessStatus.GET_SOPHY_STORY_SUCCESS, sophyStoryService.getMySophyStory(user.getUsername()));
     }
 }
--- src/main/java/org/sophy/sophy/domain/dto/SophyStoryRequestDto.java
+++ src/main/java/org/sophy/sophy/domain/dto/SophyStoryRequestDto.java
@@ -1,5 +1,6 @@
 package org.sophy.sophy.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,6 +9,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Builder
 public class SophyStoryRequestDto {
+    @Schema(description = "연도", example = "2023")
     private Integer year;
+    @Schema(description = "월", example = "7")
     private Integer month;
 }
