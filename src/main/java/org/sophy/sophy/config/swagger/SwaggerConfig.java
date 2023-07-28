145b6996736c1a19469e5cf774859ad261726fd9
dong2ast <kdh01132006@naver.com>
2023-07-28 19:16:02 +0900

    fix: swagger에 cors 관련 문제 해결 메소드 추가

--- src/main/java/org/sophy/sophy/config/swagger/SwaggerConfig.java
+++ src/main/java/org/sophy/sophy/config/swagger/SwaggerConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -34,6 +35,7 @@ public class SwaggerConfig {
             .version("1.0.0");
 
         return new OpenAPI()
+            .addServersItem(new Server().url("/"))
             .components(new Components())
             .info(info);
     }
