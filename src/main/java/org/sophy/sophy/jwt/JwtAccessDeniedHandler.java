f89a8c07db26004228777c2e384874f7963e0f48
dong2ast <kdh01132006@naver.com>
2023-07-28 14:06:28 +0900

    add: access 토큰과 권한 설정 관련 401 403 에러 예외처리 완료

--- src/main/java/org/sophy/sophy/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/org/sophy/sophy/jwt/JwtAccessDeniedHandler.java
@@ -1,5 +1,8 @@
 package org.sophy.sophy.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.sophy.sophy.common.dto.ApiResponseDto;
+import org.sophy.sophy.exception.ErrorStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -12,9 +15,13 @@ import java.io.IOException;
 //AccessDeniedException은 access권한 없는 페이지에 접속할 때 발생하는 SpringSecurity Error(예외처리 인터페이스)
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(mapper.writeValueAsString(ApiResponseDto.error(ErrorStatus.FORBIDDEN_USER_EXCEPTION, ErrorStatus.FORBIDDEN_USER_EXCEPTION.getMessage())));
     }
 }
--- src/main/java/org/sophy/sophy/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/org/sophy/sophy/jwt/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,8 @@
 package org.sophy.sophy.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.sophy.sophy.common.dto.ApiResponseDto;
+import org.sophy.sophy.exception.ErrorStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -12,10 +15,14 @@ import java.io.IOException;
 //이것도 마찬가지로 SpringSecurity에서 발생시키는 Exception 처리 인터페이스
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         //유효한 자격증명을 제공하지 않고 접근하려 할 때 401
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(mapper.writeValueAsString(ApiResponseDto.error(ErrorStatus.INVALID_ACCESS_TOKEN_EXCEPTION, ErrorStatus.INVALID_ACCESS_TOKEN_EXCEPTION.getMessage())));
     }
 }
