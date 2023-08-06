7a7cab9222b09d104ecfa815bc3e1cb9537dda56
dong2ast <kdh01132006@naver.com>
2023-08-06 15:59:30 +0900

    refactor: 공간 운영자 => 승인 대기중 북토크 조회 로직 간결화

--- src/main/java/org/sophy/sophy/infrastructure/BooktalkRepository.java
+++ src/main/java/org/sophy/sophy/infrastructure/BooktalkRepository.java
@@ -1,12 +1,15 @@
 package org.sophy.sophy.infrastructure;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import org.sophy.sophy.domain.Booktalk;
 import org.sophy.sophy.domain.enumerate.BooktalkStatus;
 import org.sophy.sophy.domain.enumerate.City;
 import org.sophy.sophy.exception.ErrorStatus;
 import org.sophy.sophy.exception.model.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface BooktalkRepository extends JpaRepository<Booktalk, Long> {
@@ -22,4 +25,9 @@ public interface BooktalkRepository extends JpaRepository<Booktalk, Long> {
             .orElseThrow(() -> new NotFoundException(ErrorStatus.NOT_FOUND_BOOKTALK_EXCEPTION,
                 ErrorStatus.NOT_FOUND_BOOKTALK_EXCEPTION.getMessage()));
     }
+
+    @Query("select o.recruitScheduledBooktalks from Member m"
+        + " join m.operatorProperty o"
+        + " where m.email = :email")
+    List<Booktalk> getWatingBooktalks(@Param("email") String email);
 }
