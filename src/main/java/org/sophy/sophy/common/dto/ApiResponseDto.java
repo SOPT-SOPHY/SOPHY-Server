2bcf5b90eb03097a0976054123aeeef8fd0468f3
dong2ast <kdh01132006@naver.com>
2023-08-07 10:47:58 +0900

    fix: generic raw타입으로 반환하지 않고 와일드카드 타입을 사용해 반환

--- src/main/java/org/sophy/sophy/common/dto/ApiResponseDto.java
+++ src/main/java/org/sophy/sophy/common/dto/ApiResponseDto.java
@@ -16,7 +16,7 @@ public class ApiResponseDto<T> {
     private final String message;
     private T data;
 
-    public static ApiResponseDto success(SuccessStatus successStatus) {
+    public static ApiResponseDto<?> success(SuccessStatus successStatus) {
         return new ApiResponseDto<>(successStatus.getHttpStatus().value(),
             successStatus.getMessage());
     }
@@ -26,15 +26,15 @@ public class ApiResponseDto<T> {
             successStatus.getMessage(), data);
     }
 
-    public static ApiResponseDto error(ErrorStatus errorStatus) {
+    public static ApiResponseDto<?> error(ErrorStatus errorStatus) {
         return new ApiResponseDto<>(errorStatus.getHttpStatus().value(), errorStatus.getMessage());
     }
 
-    public static ApiResponseDto error(ErrorStatus errorStatus, String message) {
+    public static ApiResponseDto<?> error(ErrorStatus errorStatus, String message) {
         return new ApiResponseDto<>(errorStatus.getHttpStatusCode(), message);
     }
 
-    public static ApiResponseDto error(Integer httpStatus, String message) {
+    public static ApiResponseDto<?> error(Integer httpStatus, String message) {
         return new ApiResponseDto<>(httpStatus, message);
     }
 }
