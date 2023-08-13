118e176c380569cee7dc7e17490214f8e57cecc8
dong2ast <kdh01132006@naver.com>
2023-08-13 15:51:05 +0900

    feat: 소셜 로그인 실패 handler 추가

---
+++ src/main/java/org/sophy/sophy/config/auth/common/OAuth2LoginFailureHandler.java
@@ -0,0 +1,23 @@
+package org.sophy.sophy.config.auth.common;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+        AuthenticationException exception) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.getWriter().write("소셜 로그인이 실패하였습니다.");
+        log.error("소셜 로그인에 실패했습니다. 에러메세지 : {}", exception.getMessage());
+    }
+}
