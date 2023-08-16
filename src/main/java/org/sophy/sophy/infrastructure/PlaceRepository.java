5b4604bb8e2160cb56d42d6be6ab80db390723a6
Suhyeon <70002218+onpyeong@users.noreply.github.com>
2023-08-16 09:07:05 +0900

    Refactor/#78 multiple selection city (#81)
    * refactor: in 사용하여 지역 중복 선택 가능하도록 변경
    
    * del: 필요없는 예외처리 삭제

--- src/main/java/org/sophy/sophy/infrastructure/PlaceRepository.java
+++ src/main/java/org/sophy/sophy/infrastructure/PlaceRepository.java
@@ -1,22 +1,23 @@
 package org.sophy.sophy.infrastructure;
 
-import org.sophy.sophy.domain.enumerate.City;
+import java.util.List;
 import org.sophy.sophy.domain.Place;
+import org.sophy.sophy.domain.enumerate.City;
 import org.sophy.sophy.exception.ErrorStatus;
 import org.sophy.sophy.exception.model.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
-    List<Place> findAllByCity(City city);
+
+    List<Place> findByCityIn(List<City> cities);
 
     List<Place> findAll();
 
     default Place getPlaceById(Long placeId) {
         return this.findById(placeId)
-                .orElseThrow(() -> new NotFoundException(ErrorStatus.NOT_FOUND_PLACE_EXCEPTION, ErrorStatus.NOT_FOUND_PLACE_EXCEPTION.getMessage()));
+            .orElseThrow(() -> new NotFoundException(ErrorStatus.NOT_FOUND_PLACE_EXCEPTION,
+                ErrorStatus.NOT_FOUND_PLACE_EXCEPTION.getMessage()));
     }
 }
--- src/main/java/org/sophy/sophy/service/PlaceService.java
+++ src/main/java/org/sophy/sophy/service/PlaceService.java
@@ -1,5 +1,7 @@
 package org.sophy.sophy.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.sophy.sophy.controller.dto.response.PlaceResponseDto;
 import org.sophy.sophy.domain.Member;
@@ -14,9 +16,6 @@ import org.sophy.sophy.infrastructure.MemberRepository;
 import org.sophy.sophy.infrastructure.PlaceRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -42,14 +41,14 @@ public class PlaceService {
         return PlaceResponseDto.of(placeRepository.save(place));
     }
 
-    public List<PlaceResponseDto> getPlacesByCity(City city) {
+    public List<PlaceResponseDto> getPlacesByCities(List<City> cities) {
         //의정부 시이면 전체 조회
-        if (city == City.UIJEONGBU_SI) {
+        if (cities.contains(City.UIJEONGBU_SI)) {
             return placeRepository.findAll().stream()
                 .map(PlaceResponseDto::of)
                 .collect(Collectors.toList());
         }
-        return placeRepository.findAllByCity(city).stream()
+        return placeRepository.findByCityIn(cities).stream()
             .map(PlaceResponseDto::of)
             .collect(Collectors.toList());
     }
