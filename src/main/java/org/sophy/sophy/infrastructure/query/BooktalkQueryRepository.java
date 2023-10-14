bef82e5662719fdeabc4fd7e6853f4c0dacdad84
onpyeong <gsh447114@gmail.com>
2023-10-14 14:07:48 +0900

    modify: 북토크 리스트 조회 시 모집 예정, 모집 중인 북토크 조회로 수정

--- src/main/java/org/sophy/sophy/infrastructure/query/BooktalkQueryRepository.java
+++ src/main/java/org/sophy/sophy/infrastructure/query/BooktalkQueryRepository.java
@@ -13,25 +13,33 @@ import org.springframework.data.repository.query.Param;
 public interface BooktalkQueryRepository extends JpaRepository<Booktalk, Long> {
 
     @Query("select new org.sophy.sophy.domain.dto.booktalk.response.BooktalkResponseDto("
-        + " b.id, b.preliminaryInfo, b.title, m.name, b.startDate, b.endDate, p.name, b.participantNum, b.maximum, b.booktalkImageUrl)"
+        + " b.id, b.preliminaryInfo, b.title, m.name, b.startDate, b.endDate, p.name, b.participantNum, b.maximum, b.booktalkImageUrl, b.booktalkStatus)"
         + " from Booktalk b"
         + " join b.member m"
         + " join b.place p"
-        + " where b.booktalkStatus = :booktalkStatus")
-    List<BooktalkResponseDto> findBooktalkResponseDto(
-        @Param("booktalkStatus") BooktalkStatus booktalkStatus);
+        + " where b.booktalkStatus in :booktalkStatuses")
+    List<BooktalkResponseDto> findBooktalkResponseDto(@Param("booktalkStatuses") List<BooktalkStatus> booktalkStatuses);
 
-    @Query(
-        "select new org.sophy.sophy.domain.dto.booktalk.response.BooktalkResponseDto("
-            + " b.id, b.preliminaryInfo, b.title, m.name, b.startDate, b.endDate, p.name, b.participantNum, b.maximum, b.booktalkImageUrl)"
-            + " from Booktalk b"
-            + " join b.member m"
-            + " join b.place p"
-            + " where b.city in :cities"
-            + " and b.booktalkStatus = :booktalkStatus"
-            + " order by b.startDate")
-    List<BooktalkResponseDto> findBooktalkResponseDto(@Param("cities") List<City> cities,
-        @Param("booktalkStatus") BooktalkStatus booktalkStatus); //count 쿼리는 매우 무거운데 이거를 참가자 리스트를 불러와 count를 세는게 맞나?
+//    @Query("select new org.sophy.sophy.domain.dto.booktalk.response.BooktalkResponseDto("
+//        + " b.id, b.preliminaryInfo, b.title, m.name, b.startDate, b.endDate, p.name, b.participantNum, b.maximum, b.booktalkImageUrl)"
+//        + " from Booktalk b"
+//        + " join b.member m"
+//        + " join b.place p"
+//        + " where b.booktalkStatus = :booktalkStatus")
+//    List<BooktalkResponseDto> findBooktalkResponseDto(
+//        @Param("booktalkStatus") BooktalkStatus booktalkStatus);
+//
+//    @Query(
+//        "select new org.sophy.sophy.domain.dto.booktalk.response.BooktalkResponseDto("
+//            + " b.id, b.preliminaryInfo, b.title, m.name, b.startDate, b.endDate, p.name, b.participantNum, b.maximum, b.booktalkImageUrl)"
+//            + " from Booktalk b"
+//            + " join b.member m"
+//            + " join b.place p"
+//            + " where b.city in :cities"
+//            + " and b.booktalkStatus = :booktalkStatus"
+//            + " order by b.startDate")
+//    List<BooktalkResponseDto> findBooktalkResponseDto(@Param("cities") List<City> cities,
+//        @Param("booktalkStatus") BooktalkStatus booktalkStatus); //count 쿼리는 매우 무거운데 이거를 참가자 리스트를 불러와 count를 세는게 맞나?
 
     //sql의 in절을 통해 모집중과 모집 마감 북토크들을 한번에 조회
     @Query("select b from Booktalk b where b.booktalkStatus in :booktalkStatuses")
